b5997cb8b14a31cd7564ce41b3ac4ef11f298d0c
Tazim381 <jucse28.381@gmail.com>
2023-06-25 14:20:15 +0600

    google added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      color:{
+        'bangla':'#2062CF',
+      },
+    },
+  },
+  plugins: [],
+}
+
